efe529d6a024be37f9e01bb744a98d3f8297dbdd
FL <fl50000@hotmail.com>
2021-11-02 10:55:03 -0300

    Essentials v19 version

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# Roaming Icon.
+# Roaming Icon
 This script is for Pokémon Essentials. It displays icons on map for roaming pokémon.
 
 ## Screens
 ![](Screens/screen.png)
 
 ## Compatibility
-Works only on Essentials v17, v17.1, v17.2, v18 and v18.1.
+Doesn't work on Essentials versions older than v19. To see other Essentials versions, look at [all branches](../../branches/all).
 
 ## Installation
 Follow the [Script](/Script.rb) instructions. The sample script files are in [Files folder](/Files).
--- Script.rb
+++ Script.rb
@@ -18,32 +18,32 @@
 class PokemonRegionMap_Scene
   def drawRoamingPosition(mapindex)
     for roamPos in $PokemonGlobal.roamPosition
-      roamingData = RoamingSpecies[roamPos[0]]
+      roamingData = Settings::ROAMING_SPECIES[roamPos[0]]
       active = $game_switches[roamingData[2]] && (
         $PokemonGlobal.roamPokemon.size <= roamPos[0] || 
         $PokemonGlobal.roamPokemon[roamPos[0]]!=true
       )
       next if !active
-      species=getID(PBSpecies,roamingData[0])
-      next if !species || species<=0
-      pokepos = $game_map ? pbGetMetadata(roamPos[1],MetadataMapPosition) : nil 
+      speciesData=GameData::Species.try_get(roamingData[0])
+      next if !speciesData
+      pokepos = GameData::MapMetadata.try_get(roamPos[1])&.town_map_position
       next if mapindex!=pokepos[0]
       x = pokepos[1]
       y = pokepos[2]
-      @sprites["roaming#{species}"] = IconSprite.new(0,0,@viewport)
-      @sprites["roaming#{species}"].setBitmap(getRoamingIcon(species))
-      @sprites["roaming#{species}"].x = -SQUAREWIDTH/2+(x*SQUAREWIDTH)+(
+      @sprites["roaming#{speciesData.id_number}"] = IconSprite.new(0,0,@viewport)
+      @sprites["roaming#{speciesData.id_number}"].setBitmap(getRoamingIcon(speciesData))
+      @sprites["roaming#{speciesData.id_number}"].x = -SQUAREWIDTH/2+(x*SQUAREWIDTH)+(
         Graphics.width-@sprites["map"].bitmap.width
       )/2
-      @sprites["roaming#{species}"].y = -SQUAREHEIGHT/2+(y*SQUAREHEIGHT)+(
+      @sprites["roaming#{speciesData.id_number}"].y = -SQUAREHEIGHT/2+(y*SQUAREHEIGHT)+(
         Graphics.height-@sprites["map"].bitmap.height
       )/2
     end
   end
   
-  def getRoamingIcon(species)
-    return nil if !species
-    fileName = sprintf("Graphics/Pictures/mapPokemon%03d", species)
+  def getRoamingIcon(speciesData)
+    return nil if !speciesData
+    fileName = sprintf("Graphics/Pictures/mapPokemon%03d", speciesData.id_number)
     ret = pbResolveBitmap(fileName)
     if !ret
       fileName = "Graphics/Pictures/mapPokemon000"
